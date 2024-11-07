8e4309a4c88fc15f14fce2241450845e8e2473f3
Ezra <115100554+eb73@users.noreply.github.com>
2024-11-07 11:40:24 +0100

    add release year and 2 preprocess functions

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def load_data(character_path='data/character.metadata.tsv', movie_path='data/movie.metadata.tsv', plot_path='data/plot_summaries.txt'):
     """
@@ -35,3 +36,29 @@ def load_data(character_path='data/character.metadata.tsv', movie_path='data/mov
     plot_data = pd.read_csv(plot_path, sep='\t', header=None, names=plot_columns)
    
     return character_data, movie_data, plot_data
+
+# ==================== PREPROCESSING ====================
+
+def standardize(df: pd.DataFrame):
+    """Standardize data."""
+    numeric_df = df.select_dtypes(include=[np.number]) # select only numeric columns
+    
+    mean = numeric_df.mean(axis=0, skipna=True)
+    std = numeric_df.std(axis=0, skipna=True)
+    
+    standardized_numeric_df = (numeric_df - mean) / std
+
+    standardized_df = df.copy()
+    standardized_df[numeric_df.columns] = standardized_numeric_df
+    
+    return standardized_df
+
+def replace_nans(df: pd.DataFrame, method='mean'):
+    """Replace NaN with the specified method."""
+    if method == 'mean':
+        fill_values = df.mean(numeric_only=True, skipna=True)
+    elif method == 'median':
+        fill_values = df.median(numeric_only=True, skipna=True)
+    else:
+        raise ValueError("Method must be 'mean' or 'median'")
+    return df.fillna(fill_values)
